e6ae71e0fbeb4b07bc707a9ccc08710c356ea130
Mansi Shah <shah.mansi246@gmail.com>
2025-09-10 13:54:53 -0400

    Try sending a msg to another node

--- maelstrom-playground/main.go
+++ maelstrom-playground/main.go
@@ -7,8 +7,33 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type Message struct {
+	Type      string `json:"type"`
+	Value     any    `json:"value"`
+	MessageID int    `json:"msg_id"`
+}
+
 func main() {
+	counter := 0
 	n := maelstrom.NewNode()
+
+	node_ids := n.NodeIDs()
+
+	for _, k := range node_ids {
+		msg := Message{
+			Type:      "test",
+			Value:     "Hello",
+			MessageID: counter,
+		}
+		jsonBody, err := json.Marshal(msg)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		n.Send(k, jsonBody)
+		counter = counter + 1
+	}
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -17,6 +42,17 @@ func main() {
 		}
 
 		body["type"] = "echo_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("test", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "test_ok"
 
 		return n.Reply(msg, body)
 	})
